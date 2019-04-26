120517cfc7c69b33a59bc0d575f824e2f6f6c589
Tae Hyon Whang <>
2019-04-25 23:15:11 -0400

    wip snapshot

--- src/main.rs
+++ src/main.rs
@@ -133,8 +133,7 @@ pub mod day3 {
         for l in INPUT.lines() {
             let sx = l
                 .split(&['#', ' ', ',', ':', 'x'][..])
-                .map(str::parse)
-                .filter_map(Result::ok)
+                .filter_map(|s| s.parse().ok())
                 .collect::<Vec<i32>>();
             m.insert(sx[0], sx[1..].to_vec());
         }
@@ -149,11 +148,64 @@ pub mod day3 {
     }
 }
 
+pub mod day4 {
+    use std::collections::HashMap;
+
+    const INPUT: &str = include_str!("./2018/day4.txt");
+
+    pub fn parse(s: &str) -> (&str, i32, i32) {
+        let v = s.split(&[' ', ':', ']'][..]).collect::<Vec<_>>();
+        (v[4], v[2].parse().unwrap(), v[6].parse().unwrap())
+    }
+
+    pub fn part1() -> i32 {
+        let mut v = INPUT.lines().collect::<Vec<_>>();
+        v.sort();
+
+        // for (i, x) in v.iter().enumerate() {
+        //     if i > 5 {
+        //         break;
+        //     } else {
+        //         println!("{}", x);
+        //     }
+        // }
+
+        let mut hm = HashMap::new();
+        let mut t0: i32 = 0;
+        let mut id = 0;
+        for ln in v {
+            let (m, t, k) = parse(ln);
+
+            // println!("m={} t={} id={}", m, t, id);
+            // println!("{:?}", hm);
+
+            if m == "Guard" {
+                id = k;
+                hm.insert(id, 0);
+            } else if m == "falls" {
+                t0 = t;
+            } else if m == "wakes" {
+                hm.insert(id, hm[&id] + t - t0);
+            } else {
+                panic!()
+            }
+        }
+
+        panic!()
+    }
+
+    pub fn part2() {
+        panic!()
+    }
+}
+
 fn main() {
-    println!("{}", day1::part1());
-    println!("{}", day1::part2());
-    println!("{}", day2::part1());
-    println!("{}", day2::part2());
-    println!("{}", day3::part1());
-    println!("{}", day3::part2());
+    // println!("{}", day1::part1());
+    // println!("{}", day1::part2());
+    // println!("{}", day2::part1());
+    // println!("{}", day2::part2());
+    // println!("{}", day3::part1());
+    // println!("{}", day3::part2());
+
+    // println!("{}", day4::part1());
 }

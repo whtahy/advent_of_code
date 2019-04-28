521be953568fcebff39f2ccb98748a4f1102d7e3
Tae Hyon Whang <>
2019-04-28 05:03:26 -0400

    Bugfix: day1 part2, Add: benchmarks, doctests

--- benches/bench.rs
+++ benches/bench.rs
@@ -7,36 +7,41 @@ use criterion::Criterion;
 // fn day1p1(c: &mut Criterion) {
 //     c.bench_function("day1p1", |b| b.iter(day1::part1));
 // }
+// criterion_group!(b_day1p1, day1p1);
 
 // fn day1p2(c: &mut Criterion) {
 //     c.bench_function("day1p2", |b| b.iter(day1::part2));
 // }
+// criterion_group!(b_day1p2, day1p2);
 
 // fn day2p1(c: &mut Criterion) {
 //     c.bench_function("day2p1", |b| b.iter(day2::part1));
 // }
+// criterion_group!(b_day2p1, day2p1);
 
 // fn day2p2(c: &mut Criterion) {
 //     c.bench_function("day2p2", |b| b.iter(day2::part2));
 // }
+// criterion_group!(b_day2p2, day2p2);
 
-// fn day3p1(c: &mut Criterion) {
-//     c.bench_function("day3p1", |b| b.iter(day3::part1));
-// }
-
-// fn day3p2(c: &mut Criterion) {
-//     c.bench_function("day3p2", |b| b.iter(day3::part2));
-// }
-
-fn day4p1(c: &mut Criterion) {
-    c.bench_function("day4p1", |b| b.iter(day4::part1));
+fn day3p1(c: &mut Criterion) {
+    c.bench_function("day3p1", |b| b.iter(day3::part1));
 }
+criterion_group!(b_day3p1, day3p1);
 
-fn day4p2(c: &mut Criterion) {
-    c.bench_function("day4p2", |b| b.iter(day4::part2));
+fn day3p2(c: &mut Criterion) {
+    c.bench_function("day3p2", |b| b.iter(day3::part2));
 }
+criterion_group!(b_day3p2, day3p2);
 
-criterion_group!(b_day4p1, day4p1);
-criterion_group!(b_day4p2, day4p2);
+// fn day4p1(c: &mut Criterion) {
+//     c.bench_function("day4p1", |b| b.iter(day4::part1));
+// }
+// criterion_group!(b_day4p1, day4p1);
 
-criterion_main!(b_day4p1, b_day4p2);
+// fn day4p2(c: &mut Criterion) {
+//     c.bench_function("day4p2", |b| b.iter(day4::part2));
+// }
+// criterion_group!(b_day4p2, day4p2);
+
+criterion_main!(b_day3p1, b_day3p2);

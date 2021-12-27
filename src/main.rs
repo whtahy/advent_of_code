c967c5024f8cb64d191e3f4331eaf3a2cb3437a1
whtahy <user.email>
2021-12-27 09:46:57 -0500

    snapshot: refactor into multiple crates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use advent_of_code::*;
+use std::env;
+
+fn arg(i: usize) -> usize {
+    env::args().nth(i).unwrap().parse().unwrap()
+}
 
 fn main() {
-    // aoc_2015::main(1, 1);
-    // aoc_2016::main(1, 1);
-    // aoc_2017::main(1, 1);
-    // aoc_2018::main(1, 1);
-    // aoc_2019::main(1, 1);
-    aoc_2020::main(7, 2);
+    let [year, day, part] = [arg(1), arg(2), arg(3)];
+    println!("{}", advent_of_code::get(year, day, part));
 }

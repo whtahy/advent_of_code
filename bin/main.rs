3ab5bac3a5e1ffd6a0cd3d37a6b348665981e1c9
whtahy <whtahy@users.noreply.github.com>
2022-04-27 21:18:49 -0400

    main: add cli args

--- bin/main.rs
+++ bin/main.rs
@@ -1,4 +1,4 @@
-// use std::env;
+use std::env;
 use std::panic;
 
 type Day = [fn() -> String; 2];
@@ -18,19 +18,33 @@ fn get(year: usize, day: usize, part: usize) -> String {
     TABLE_OF_CONTENTS[year - 2015][day - 1][part - 1]()
 }
 
-fn main() {
+fn get_most_recent() {
     panic::set_hook(Box::new(|_| {}));
     for year in (2015..=2021).rev() {
         for day in (1..=25).rev() {
             for part in (1..=2).rev() {
                 match std::panic::catch_unwind(|| get(year, day, part)) {
                     Ok(ans) => {
-                        println!("{} Day{} Part{}: {}", year, day, part, ans);
+                        println!("{year} Day{day} Part{part}: {ans}");
                         return;
                     }
                     Err(_) => continue,
                 }
             }
         }
+    }
+}
+
+fn main() {
+    let args: Vec<usize> = env::args().filter_map(|s| s.parse().ok()).collect();
+
+    if args.len() == 3 {
+        let year = args[0];
+        let day = args[1];
+        let part = args[2];
+        let ans = get(year, day, part);
+        println!("{year} Day{day} Part{part}: {ans}");
+    } else {
+        get_most_recent()
     }
 }

1bb54678e29d998d6c8bdbdf2878807aebc30097
unknown <whtahy@users.noreply.github.com>
2022-12-03 14:28:56 -0500

    2022 scaffolding

---
+++ aoc_2022/lib.rs
@@ -0,0 +1,326 @@
+shared::table_of_contents!();
+
+pub mod day1 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day2 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day3 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day4 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day5 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day6 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day7 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day8 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day9 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day10 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day11 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day12 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day13 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day14 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day15 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day16 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day17 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day18 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day19 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day20 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day21 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day22 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day23 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day24 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
+
+pub mod day25 {
+    shared::input!();
+    shared::test!(); // examples:
+
+    pub fn part1() -> String {
+        todo!()
+    }
+
+    pub fn part2() -> String {
+        todo!()
+    }
+}
